e0782c1b8571981784f7b013981aa1d5ee5e9d02
Joseph Granville <jwgranville@gmail.com>
2023-04-06 18:35:37 +0000

    Container won't run; restarting

--- rl_demo_wk1.py
+++ rl_demo_wk1.py
@@ -13,7 +13,7 @@ also use rand.gauss(0, 1) instead of i
 
 import random as rand
 
-N = 2
+N = 4
 
 def average1():
     sum = 0
@@ -31,12 +31,36 @@ def average2():
 def average3():
     avg = 0
     for i in range(1, N):
-        avg = i #fix this
+        avg = avg + ( i - avg ) / i
     print('avg3', avg)
+
+def average1r():
+    sum = 0
+    for i in range(1, N):
+        sum += rand.gauss(0, 1)
+    avg = sum / (N-1)
+    print('avg1r', avg)
+
+def average2r():
+    avg = 0
+    for i in range(1, N):
+        avg = avg * (i-1)/i + rand.gauss(0, 1) / i
+    print('avg2r', avg)
+
+def average3r():
+    avg = 0
+    for i in range(1, N):
+        avg = avg + ( rand.gauss(0, 1) - avg ) / i
+    print('avg3r', avg)
+
+
 
 
 if __name__ == '__main__':
     average1()
     average2()
     average3()
+    average1r()
+    average2r()
+    average3r()
     
